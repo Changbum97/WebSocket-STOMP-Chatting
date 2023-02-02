a1add54dd93bea413f185f6cf7c51df18f520969
Changbum97 <chb2005@naver.com>
2023-02-02 16:33:27 +0900

    FEAT: 엔터키치면 입력 기능 추가

--- src/main/java/study/chattingwithdb/domain/entity/ChatMessage.java
+++ src/main/java/study/chattingwithdb/domain/entity/ChatMessage.java
@@ -2,8 +2,11 @@ package study.chattingwithdb.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -11,6 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Builder
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class ChatMessage {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +24,10 @@ public class ChatMessage {
     private Long writerId;
     private Boolean readCheck;   // 상대가 읽었는지 체크
     private Long roomId;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chatRoom_id")
